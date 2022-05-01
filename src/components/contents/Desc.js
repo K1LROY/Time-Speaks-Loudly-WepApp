217d9e080a6959f67f849d58c07fe192150623be
Supatarchai <Luck-y_se7en@hotmail.com>
2022-05-01 12:28:29 +0700

    Add Desc

---
+++ src/components/contents/Desc.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../../styles/contentStyles/desc.css";
+
+export default function Desc() {
+  return (
+    <div id="desc" class="desc mt-5 py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 offset-sm-2 text-center">
+            <div class="text-muted ">Creative Enough</div>
+            <h2 class="font-weight-bold mb-3  white">What is Retro Themes ?</h2>
+            <p class="lead ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+              sed, necessitatibus deserunt magnam voluptatibus tempora incidunt
+              odit dolore delectus! Earum sed, necessitatibus deserunt magnam
+              voluptatibus tempora incidunt odit dolore delectus. Ut autem
+              veritatis!
+            </p>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-sm-4 ">
+            <div class="h4">Made with HTML5</div>
+            <p class="font-weight-light text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+              distinctio rerum, adipisci itaque ullam quos libero iusto non ut.
+            </p>
+          </div>
+          <div class="col-sm-4 ">
+            <div class="h4">Tested in Browsers</div>
+            <p class="font-weight-light text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+              distinctio rerum, adipisci itaque ullam quos libero iusto non ut.
+            </p>
+          </div>
+          <div class="col-sm-4 ">
+            <div class="h4">Scroll Animated</div>
+            <p class="font-weight-light text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
+              distinctio rerum, adipisci itaque ullam quos libero iusto non ut.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Desc from "../components/contents/Desc";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Desc />
     </div>
   );
 }
