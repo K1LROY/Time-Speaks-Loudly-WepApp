445ae7339cb68bf9050c0b57b0e85006052430bf
Supatarchai <Luck-y_se7en@hotmail.com>
2022-05-01 12:01:25 +0700

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/header.css";
 
 export default function Header() {
@@ -49,6 +50,43 @@ export default function Header() {
           <li>Locate</li>
         </ul>
       </nav>
+      <div class="intro mt-5 py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 text-center">
+              <h1 class="">Time Speaks Loudly</h1>
+              <p class="lead">Lorem ipsum dolor sit amet odor lemo</p>
+              <Link to="#" class="btn-link">
+                Watch Our Video <i class="fa fa-play"></i>
+              </Link>
+            </div>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-sm-12 text-center">
+              <div class="product-wrapper">
+                <img
+                  src="images/product-600x1200-3.png"
+                  class="scroll-move"
+                  alt="Product Preview"
+                />
+
+                <img
+                  src="images/product-600x1200-1.png"
+                  class="scroll-stable"
+                  alt="Product Preview"
+                />
+
+                <img
+                  src="images/product-600x1200-2.png"
+                  class="scroll-move"
+                  alt="Product Preview"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
